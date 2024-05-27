b1f95d5c1cc930bee40945731b8bc791618d82e2
AuriTargino <auritargino79@hotmail.com>
2024-05-27 17:27:28 +0100

    testando commit netbeans

---
+++ TesteJava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.testejava;
+
+/**
+ *
+ * @author PC SSD
+ */
+public class TesteJava {
+
+    public static void main(String[] args) {
+        System.out.println("Testando commit no netbeans");
+    }
+}
